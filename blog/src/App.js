16327e0de854bf6075e5785cb0262fdc36b167f3
Abhinav Rai <111674442+AbhinavRai2004@users.noreply.github.com>
2023-07-08 19:35:39 +0530

    updated app.js File

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,86 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
+import Student from './Student';
+
+// function App() {
+//     return ( <
+//         div className = "App" >
+//         <
+//         h1 > Hello World < /h1>  <
+//         User / > <
+//         /div>
+//     );
+// }
+
+// function User() {
+//     return ( <
+//         h1 > User Component < /h1>
+//     )
+// }
+
+
+// Event.
+
+
+// function App() {
+//     let data = "Abhinav Rai";
+
+//     function clickEvent() {
+//         data = "Abhi";
+//         alert(data);
+//     }
+//     return ( <
+//         div className = "App" >
+//         <
+//         h1 > { data } < /h1>  <
+//         button onClick = { clickEvent } > Click Me < /button> < /
+//         div >
+//     );
+// }
+// export default App;
+
+
+// states.
+
+// function App() {
+//     const [data, setData] = useState(0);
+
+//     function updateData() {
+//         setData(data + 1);
+//     }
+//     return ( <
+//         div className = "App" >
+//         <
+//         h1 > { data } < /h1>  <
+//         button onClick = { updateData } > UpdateData < /button> < /
+//         div >
+//     );
+// }
+
+
+// export default App;
+
+
+
+// Props in reactJs.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState("Abhinav")
+    return ( <
+        div className = "App" >
+        <
+        h1 > Props In React: ) < /h1>  <
+    Student name = { data }
+    email = { "abh12@gmail.com" }
+    />   <
+    button onClick = {
+        () => { setData("Rai") }
+    } > UpdateData < /button>< /
+    div >
+);
 }
+
 
 export default App;
---
+++ blog/src/Student.js
@@ -0,0 +1,13 @@
+function Student(props) {
+    return ( <
+        div style = {
+            { backgroundColor: "skyblue", margin: 10 }
+        } >
+        <
+        h1 > Hello { props.name } < /h1>  <
+        h2 > Email: { props.email } < /h2>< /
+        div >
+    )
+}
+
+export default Student
